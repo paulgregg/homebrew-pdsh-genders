fc1d2b94f5e2ffbee0e7f6fb3a643cd5ffa1b4a5
Paul Gregg <pgregg@proofpoint.com>
2021-12-14 15:50:42 +0000

    Bump to 2.34

--- pdsh.rb
+++ pdsh.rb
@@ -1,15 +1,20 @@
 class Pdsh < Formula
   desc "Efficient rsh-like utility, for using hosts in parallel"
   homepage "https://github.com/chaos/pdsh"
-  url "https://github.com/chaos/pdsh/releases/download/pdsh-2.33/pdsh-2.33.tar.gz"
-  sha256 "7368087429d6269f0a6313c406ef38c6a6a947bc003ca7368fc6481b139d942f"
-  revision 1
-  head "https://github.com/chaos/pdsh.git"
+  url "https://github.com/chaos/pdsh/releases/download/pdsh-2.34/pdsh-2.34.tar.gz"
+  sha256 "b47b3e4662736ef44b6fe86e3d380f95e591863e69163aa0592e9f9f618521e9"
+  license "GPL-2.0"
+  head "https://github.com/chaos/pdsh.git", branch: "master"
 
   bottle do
-    sha256 "69d61fe9f4a0ba006e4e5061acaee0b1d9da2b66f8b02af74d510ebea2018e34" => :mojave
-    sha256 "5fa7e12d3877247c072e3d393efc6e54bb295f9a6e837402c693f3805990a74b" => :high_sierra
-    sha256 "d70f3e18351291dcd04bcc6f59d15aabdeec44cee21e1729205ef9862f97099b" => :sierra
+    sha256 arm64_monterey: "4f4123b72684e34e5e78b67269da1aae6e29e0ae60d1f144754d521289230d5a"
+    sha256 arm64_big_sur:  "08092fc19817b2eb4fbbb1ffe7849246f7a8c0cfc1b3493d631f41b77ab68de6"
+    sha256 monterey:       "6f62900a3ebb6760631932fab7cf15c6cc3a12f2c2c8d390a46710f157716baf"
+    sha256 big_sur:        "c9865e6ec25dd5d76c412919c161377a73e0e72f53b97c433488377ea6c69ece"
+    sha256 catalina:       "db103afd01523d00761df3c077b309ffeaa7e816a538ab9b739fac7b58a8171b"
+    sha256 mojave:         "d5ce164360edacbda30b059e8964fc6e4c886adc5f63218a37667756419ef51a"
+    sha256 high_sierra:    "4063ea4d575eef74e2af6993a74658df6c48e42b81df8a77a49aee745c7527a0"
+    sha256 x86_64_linux:   "02ddcbc57a480f8a5f049e6f50de9621b7eb6f84ca6e52ecd91a8747035641b6"
   end
 
   depends_on "readline"
